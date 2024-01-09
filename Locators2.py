3d526d5e62e1b31666ddaaba5555147584706442
Hasanul-Banna <hbshovon776@gmail.com>
2024-01-09 11:58:03 +0600

    Locators

--- Locators2.py
+++ Locators2.py
@@ -9,20 +9,18 @@ cap:Dict[str, Any]={
     "platformName": "android",
     "automationName": "UIAutomator2",
     "deviceName": "android"
-
-
 }
 
 url = 'http://localhost:4723'
 
 driver = webdriver.Remote(url, options=AppiumOptions().load_capabilities(cap))
 
-el = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value='YouTube')
-
+el = driver.find_element(by=AppiumBy.XPATH, value='//android.widget.TextView[@content-desc="YouTube"]')
 el.click()
 time.sleep(5)
 
 driver.find_element(by=AppiumBy.XPATH, value='//android.widget.ImageView[@content-desc="Search"]').click()
-time.sleep(5)
 driver.find_element(by=AppiumBy.XPATH, value='//*[@text="Search YouTube"]').send_keys("Merciful Servant")
+
+# time.sleep(5)
 # driver.quit()
